604ee063a08f9074c7ecc3c677a3b170d21ffe83
Takeshi Kondo <ncukondo@gmail.com>
2019-02-14 23:15:06 +0900

    add Html copy

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,24 @@ export const copyToClip = (text: string) => {
   if (ta.parentElement) ta.parentElement.removeChild(ta);
 };
 
+export const copyHtmlToClip = (html: string) => {
+  const el = document.createElement('div');
+  el.innerHTML = html;
+  document.body.appendChild(el);
+  const range = document.createRange();
+  range.selectNode(el);
+
+  const selection = getSelection();
+  selection.removeAllRanges();
+  selection.addRange(range);
+
+  document.execCommand('copy');
+
+  selection.removeAllRanges();
+  document.execCommand('copy');
+  if (el.parentElement) el.parentElement.removeChild(el);
+};
+
 export const copyFromClip = (): string => {
   const ta = document.createElement('textarea');
   document.body.appendChild(ta);
